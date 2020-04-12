0c687c6c899c654b72c9897b0e35b284dfd6a62f
jggalmeida <j.g.almeida@hotmail.com>
2020-04-12 15:21:51 -0300

    Entidade

---
+++ ac1-poo2/src/main/java/com/example/ac1poo2/Repository/FuncionarioRepository.java
@@ -0,0 +1,11 @@
+package com.example.ac1poo2.Repository;
+
+import com.example.ac1poo2.Entity.Funcionario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
+
+
+
+}
